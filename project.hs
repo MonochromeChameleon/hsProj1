40eddc0fe341748df4b1ec1d0a57a00ff587f723
Hugh Greenish <github@monochromechameleon.com>
2013-11-01 17:25:09 +0000

    asjfdkjsdfkuga4

--- project.hs
+++ project.hs
@@ -1,47 +1,130 @@
+-- Define data structure
+
 type Name = String
 type Phone = (String, String)
 type Phones = [Phone]
-type AddressPart = (String, String)
-type Address = [AddressPart]
-type DoB = (Int, Int, Int)
+type AddressLine = (String, String)
+type Address = [AddressLine]
+type DoB = String
 
-data Property = Name | Phones | Address | DoB 
+data Person = Person { name :: Name
+                     , phones :: Phones
+                     , address :: Address
+                     , dob :: DoB } deriving (Eq, Show)
 
-data Person = Person Name Phones Address DoB deriving Show
-data PhoneBook = PhoneBook [Person]
+data PhoneBook = PhoneBook [Person] deriving Show
 
-jim = Person "Jim" [("mobile", "01234567890"), ("home", "981287673687572378")] [] (25,2,82)
-bob = Person "Bob" [] [("line1", "My House"), ("line2", "My Street")] (0,0,0)
+-- /data structure
+
+-- Define some rudimentary test data
+
+jim = Person "Jim" [("mobile", "01234567890"), ("home", "981287673687572378")] [] "25/2/82"
+bob = Person { name = "Bob", phones = [], address = [("line1", "My House"), ("line2", "My Street")], dob = "" }
 
 pb = PhoneBook [jim, bob]
 
-getName (Person n _ _ _) = n
+-- /test data
 
-getPhones (Person _ p _ _) = p
+-- XML output functions
 
-getAddress (Person _ _ a _) = a
-
-getDoB (Person _ _ _ d) = d
-writeDoB (dd, mm, yy) = if dd > 0 && mm > 0 && yy > 0 then (show dd) ++ "/" ++ (show mm) ++ "/" ++ (show yy) else ""
-
-{-get :: Person -> String -> Property
-get (Person n p a d) prop = case prop!!0 of
-  'n' -> n
-  'p' -> p
-  'a' -> a
-  'd' -> d
-  otherwise -> error "undefined property"
--}
 xout t s = if s == "" then "" else "<" ++ t ++ ">" ++ s ++ "</" ++ t ++ ">"
 
-xoutName p = xout "name" (getName p)
+xoutName p = xout "name" (name p)
 xoutPhone (t, n) = xout t n
-xoutPhones p = xout "phones" (concat (map xoutPhone (getPhones p)))
+xoutPhones p = xout "phones" (concat (map xoutPhone (phones p)))
 xoutAddressLine (t,l) = xout t l
-xoutAddress p = xout "address" (concat (map xoutAddressLine (getAddress p)))
-xoutDoB p = xout "dob" (writeDoB(getDoB p))
+xoutAddress p = xout "address" (concat (map xoutAddressLine (address p)))
+xoutDoB p = xout "dob" (dob p)
 
 writePerson :: Person -> String
 writePerson p = xout "person" ((xoutName p) ++ (xoutPhones p) ++ (xoutAddress p) ++ (xoutDoB p))
 
 writePhoneBook (PhoneBook ps) = concat (map writePerson ps)
+
+-- /XML output
+
+-- Parsing functions
+
+notCloseTag x = x /= '>'
+notOpenTag x = x /= '<'
+
+parseNode :: String -> (String, String)
+parseNode (x:xs) = span notCloseTag xs
+
+parseSimpleNode :: String -> ((String, String), String)
+parseSimpleNode xs = ((fst node, fst value), tail $ dropWhile notCloseTag $ snd value)
+    where node = parseNode xs
+          value = span notOpenTag $ tail $ snd node
+
+parseName :: String -> (Name, String)
+parseName xs = if take 6 xs == "<name>" then (fst result, dropWhile notOpenTag $ dropWhile notCloseTag $ snd result) else ("", xs)
+    where result = span notOpenTag $ tail $ snd (parseNode xs)
+
+parsePhone :: String -> (Phone, String)
+parsePhone xs = parseSimpleNode xs
+
+parsePhonesRec :: String -> [Phone] -> ([Phone], String)
+parsePhonesRec str ps = if take 9 str == "</phones>" then (ps, str) else parsePhonesRec (snd result) (ps ++ [fst result])
+    where result = parseSimpleNode str
+
+parsePhones :: String -> ([Phone], String)
+parsePhones str = if take 8 str == "<phones>" then (fst result, tail $ dropWhile notCloseTag $ snd result) else ([], str)
+    where result = parsePhonesRec (dropWhile notOpenTag $ dropWhile notCloseTag str) []
+
+parseAddressLine :: String -> (AddressLine, String)
+parseAddressLine xs = parseSimpleNode xs
+
+parseAddressRec :: String -> [AddressLine] -> ([AddressLine], String)
+parseAddressRec str ls = if take 10 str == "</address>" then (ls, str) else parseAddressRec (snd result) (ls ++ [fst result])
+    where result = parseSimpleNode str
+
+parseAddress :: String -> ([AddressLine], String)
+parseAddress str = if take 9 str == "<address>" then (fst result, tail $ dropWhile notCloseTag $ snd result) else ([], str)
+    where result = parseAddressRec (dropWhile notOpenTag $ dropWhile notCloseTag str) []
+
+parseDoB :: String -> (DoB, String)
+parseDoB xs = if take 5 xs == "<dob>" then (fst result, dropWhile notOpenTag $ dropWhile notCloseTag $ snd result) else ("", xs)
+    where result = span notOpenTag $ tail $ snd (parseNode xs)
+
+
+parsePerson :: String -> (Person, String)
+parsePerson xs = (Person { name = parsedName,
+                          phones = parsedPhones,
+                          address = parsedAddress,
+                          dob = parsedDoB }, tail $ dropWhile notCloseTag rest4)
+   where personDetails = tail $ snd $ parseNode xs
+         (parsedName, rest1) = parseName personDetails
+         (parsedPhones, rest2) = parsePhones rest1
+         (parsedAddress, rest3) = parseAddress rest2
+         (parsedDoB, rest4) = parseDoB rest3
+
+
+readPhoneBookRec str people = if take 8 str == "<person>" then readPhoneBookRec rest (people ++ [person]) else people
+   where (person, rest) = parsePerson str
+
+readPhoneBook :: String -> PhoneBook
+readPhoneBook str = PhoneBook (readPhoneBookRec str [])
+
+
+-- /Parsing
+
+-- Cack
+
+--isPersonRecord str = take 9 str /= "</person>"
+
+
+--testSomething str = takeWhile isPersonRecord str
+
+
+--splitIntoPersons str = []
+
+--readPhoneBook str = map parsePerson $ splitIntoPersons str
+
+main = do 
+  xs <- readFile "phoneBook.xml"
+
+  --let pb = readPhoneBook xs
+  return  xs
+
+
+
